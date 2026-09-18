b9eba4819bec6d853c5b694a2a515cfb8c5ed594
Fabian Schneider <fa-schneider@outlook.de>
2026-09-18 08:44:21 +0200

    Deleted code from main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <QtWidgets/QApplication>
 #include "GUI/MainWindow.h"
-//#include "GUI/MonstaWidget.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    //MonstaWidget *m;
     w.setWindowTitle("Monsta Simulator");
     w.show();
      
